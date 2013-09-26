605625085fa83969b80abdc503520ee356118b2d
比留間　大悟 <hirudai@hirumadaigo-no-MacBook-Air.local>
2013-09-27 08:16:13 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tekitoniasobu::Application.config.secret_token = '66011a6fc689e20ea26fe865ab31e221570997069c3248b67eaf718962b1ad25c9e19b92bb3b50612e07c9fee6e14fb67bb175c0b81ad8a9e42e6ad46d47175e'
